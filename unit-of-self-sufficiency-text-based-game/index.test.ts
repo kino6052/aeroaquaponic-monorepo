db24102fe792b9c7879989cd311baebb12545fc8
nino-ux <nikita.novik.ux@gmail.com>
2022-11-13 21:56:59 +0300

    Game: Add a couple of tests

--- unit-of-self-sufficiency-text-based-game/index.test.ts
+++ unit-of-self-sufficiency-text-based-game/index.test.ts
@@ -7,9 +7,13 @@ type TEvent = [TCommand, string];
 interface IState {
   input: string;
   output: string;
+  isGoogling: boolean;
+  hasReadAboutSelfSufficiency: boolean;
 }
 
 const initialState: IState = {
+  isGoogling: false,
+  hasReadAboutSelfSufficiency: false,
   input: "",
   output: `
 h1 Wake up, Neo...
@@ -19,6 +23,10 @@ p Yesterday, you started seriously thinking about what alternatives are out ther
 };
 
 const selectOutput = (state: IState) => state.output;
+const selectInput = (state: IState) => state.input;
+const selectIsGoogling = (state: IState) => state.isGoogling;
+const selectHasReadManifest = (state: IState) =>
+  state.hasReadAboutSelfSufficiency;
 
 const reduce = (event: TEvent, state: IState): IState => {
   if (event[0] === "enter" && state.input === "help") {
@@ -34,12 +42,50 @@ p The commands available can be discovered by double tapping the Tab key.
       draft.input = event[1];
     });
   }
+  if (event[0] === "suggest" && selectInput(state) === "google") {
+    return produce(state, (draft) => {
+      draft.output = `
+b Possible google commands
+ul
+    li Self-sufficiency
+`;
+    });
+  }
   if (event[0] === "suggest") {
     return produce(state, (draft) => {
       draft.output = `
 b Available commands
-i google
-  p -- Allows to find something on the internet
+p Note: You can autocomplete queries by hitting Tab. For example, enter "goo" and hit Tab key, you will get "google"
+p Then if you hit Tab twice you will get some options of what makes sense to google.
+div
+  i google
+  p Allows to find something on the internet
+  p Try writing 
+`;
+    });
+  }
+  if (
+    event[0] === "enter" &&
+    selectInput(state) === "google self-sufficiency"
+  ) {
+    return produce(state, (draft) => {
+      draft.isGoogling = true;
+      draft.hasReadAboutSelfSufficiency = true;
+      draft.output = `
+h1 Google results
+ul
+  li
+    b Unit of self-sufficiency
+`;
+    });
+  }
+  if (event[0] === "enter" && selectInput(state) === "leave") {
+    return produce(state, (draft) => {
+      draft.isGoogling = false;
+      draft.output = `
+p You read about unit of self-sufficiency and it seemed quite reasonable.
+p It seems relatively simple too, so you want to start thinking in this direction.
+p You created a todo list.
 `;
     });
   }
@@ -78,8 +124,61 @@ p The commands available can be discovered by double tapping the Tab key.
     expect(selectOutput(resultingState)).toMatchInlineSnapshot(`
 "
 b Available commands
-i google
-  p -- Allows to find something on the internet
+p Note: You can autocomplete queries by hitting Tab. For example, enter "goo" and hit Tab key, you will get "google"
+p Then if you hit Tab twice you will get some options of what makes sense to google.
+div
+  i google
+  p Allows to find something on the internet
+  p Try writing 
+"
+`);
+  });
+
+  it("should suggest what to google", () => {
+    const resultingState = compose(initialState)([
+      ["change", "google"],
+      ["suggest", ""],
+    ]);
+    expect(selectOutput(resultingState)).toMatchInlineSnapshot(`
+"
+b Possible google commands
+ul
+    li Self-sufficiency
+"
+`);
+  });
+
+  it("should google self-sufficiency", () => {
+    const resultingState = compose(initialState)([
+      ["change", "google self-sufficiency"],
+      ["enter", ""],
+    ]);
+    expect(selectIsGoogling(resultingState)).toBe(true);
+    expect(selectHasReadManifest(resultingState)).toBe(true);
+    expect(selectOutput(resultingState)).toMatchInlineSnapshot(`
+"
+h1 Google results
+ul
+  li
+    b Unit of self-sufficiency
+"
+`);
+  });
+
+  it("should leave site", () => {
+    const resultingState = compose(initialState)([
+      ["change", "google self-sufficiency"],
+      ["enter", ""],
+      ["change", "leave"],
+      ["enter", ""],
+    ]);
+    expect(selectIsGoogling(resultingState)).toBe(false);
+    expect(selectHasReadManifest(resultingState)).toBe(true);
+    expect(selectOutput(resultingState)).toMatchInlineSnapshot(`
+"
+p You read about unit of self-sufficiency and it seemed quite reasonable.
+p It seems relatively simple too, so you want to start thinking in this direction.
+p You created a todo list.
 "
 `);
   });
