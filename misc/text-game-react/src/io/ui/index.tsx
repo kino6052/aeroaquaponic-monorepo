e98d2f84edc9d09a28141ccb2d8e8f6e6a1ebec7
nino-ux <nikita.novik.ux@gmail.com>
2023-01-12 12:21:13 +0300

    Game: Update

--- misc/text-game-react/src/io/ui/index.tsx
+++ misc/text-game-react/src/io/ui/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BehaviorSubject, Subject } from "rxjs";
 import { AppEventSubject, IState } from "../../bridge";
 import App from "./App";
 import "./index.css";
@@ -10,19 +11,10 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const getSpeech = (state: IState) => {
-  speechSynthesis.cancel();
-  let speakData = new SpeechSynthesisUtterance();
-  speakData.lang = "en";
-  speakData.volume = 1; // From 0 to 1
-  speakData.rate = 5; // From 0.1 to 10
-  speakData.pitch = 2; // From 0 to 2
-  speakData.text = state.output;
-  speechSynthesis.speak(speakData);
-};
+const SpeechSubject = new BehaviorSubject<string>("");
 
 export const presentationIO = (state: IState) => {
-  getSpeech(state);
+  SpeechSubject.next(state.output);
   root.render(
     <React.StrictMode>
       <App state={state} />
@@ -35,6 +27,16 @@ EventSubject.subscribe((event) => {
   const [type, id, value] = event;
   if (type === "load") {
     // TODO: Implement
+    SpeechSubject.subscribe((output) => {
+      speechSynthesis.cancel();
+      let speakData = new SpeechSynthesisUtterance();
+      speakData.text = output;
+      speakData.lang = "en";
+      speakData.volume = 1; // From 0 to 1
+      speakData.rate = 5; // From 0.1 to 10
+      speakData.pitch = 2; // From 0 to 2
+      speechSynthesis.speak(speakData);
+    });
     return;
   }
   if (type === "keyDown") {
