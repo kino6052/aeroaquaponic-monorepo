469ce7fde5b07adb16f694494412bb954a2a5eb5
nino-ux <nikita.novik.ux@gmail.com>
2023-05-13 12:10:41 +0300

    Figma Parser: Update

--- misc/figma-parser/code.ts
+++ misc/figma-parser/code.ts
@@ -1,94 +1,101 @@
-// This plugin creates 5 rectangles on the screen.
-// const numberOfRectangles = 5
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
-
-// const nodes: SceneNode[] = [];
-
-// figma.currentPage.children.forEach(({ name, type }) =>
-//   console.warn({ name, type })
-// );
-// This plugin counts the number of layers, ignoring instance sublayers,
-// in the document
-
 const BACKGROUND = "__BACKGROUND__";
 
-// Your plugin code goes here
-function reflowComponents(node: SceneNode & ChildrenMixin) {
+// Function to collect all top-level components in the page
+function collectComponents(node: SceneNode & ChildrenMixin): SceneNode[] {
   const components: SceneNode[] = [];
-  const { x, y } = node;
-
-  // Collect all top-level components in the page
   if (node.children) {
     for (const child of node.children) {
-      console.warn(child.name);
       if (child.name === BACKGROUND) continue;
       components.push(child);
     }
   }
+  return components;
+}
 
+// Function to remove all nodes with a specific name
+function removeNodesByName(
+  node: SceneNode & ChildrenMixin,
+  name: string
+): void {
   node
-    .findChildren((node) => node.name === BACKGROUND)
+    .findChildren((node) => node.name === name)
     .forEach((node) => {
       node.remove();
     });
-  const _backgroundNode = figma.createRectangle();
+}
 
+// Function to create a background node
+function createBackgroundNode(node: SceneNode & ChildrenMixin): SceneNode {
+  const _backgroundNode = figma.createRectangle();
   _backgroundNode.name = BACKGROUND;
-  // _backgroundNode.x = 0;
-  // _backgroundNode.y = 0;
-  console.warn(node.name, node.x, node.y, node.width, node.height, node);
   _backgroundNode.resize(node.width, node.height);
   node.appendChild(_backgroundNode);
+  return _backgroundNode;
+}
 
-  // Calculate new positions for components
-  let currentX = node.x;
+// Function to reposition components
+function repositionComponents(
+  node: SceneNode & ChildrenMixin,
+  components: SceneNode[],
+  flow: "row" | "column" = "row"
+): void {
+  //@ts-ignore;
+  (node as GroupNode).resize(node.parent.width, node.height);
 
-  for (const component of [...components].reverse()) {
-    try {
-      console.warn(component);
-      component.x = currentX;
-      currentX += component.width;
-      component.y = node.y;
-      // @ts-ignore
-      component.height = node.height;
-    } catch (e) {
-      console.warn(e);
+  if (flow === "row") {
+    let currentX = node.x;
+    for (const component of [...components].reverse()) {
+      try {
+        component.x = currentX;
+        currentX += component.width;
+        component.y = node.y;
+        // @ts-ignore
+        component.height = node.height;
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  } else {
+    let currentX = node.x;
+    let currentY = node.y;
+    for (const component of [...components].reverse()) {
+      try {
+        component.y = currentY;
+        currentY += component.height;
+        component.x = node.x;
+        // component.relativeTransform = []
+        // // @ts-ignore
+        // component.width = node.height;
+      } catch (e) {
+        console.warn(e);
+      }
     }
   }
+}
 
+// Function to append components to a node
+function appendComponents(
+  node: SceneNode & ChildrenMixin,
+  components: SceneNode[]
+): void {
   for (const component of components) {
     node.appendChild(component);
   }
+}
+
+// Your plugin code goes here
+function reflowComponents(node: SceneNode & ChildrenMixin) {
+  const { x, y } = node;
+
+  const components = collectComponents(node);
+  removeNodesByName(node, BACKGROUND);
+  createBackgroundNode(node);
+  repositionComponents(node, components, "row");
+  appendComponents(node, components);
 
   node.x = x;
   node.y = y;
 }
-
-let count = 0;
-function traverse(node: SceneNode & ChildrenMixin) {
-  if ("children" in node) {
-    count++;
-
-    reflowComponents(node);
-    // if (node.type !== "INSTANCE") {
-    if (!node.children) return;
-
-    for (const child of node.children) {
-      traverse(child as SceneNode & ChildrenMixin);
-      console.warn(child);
-    }
-    // }
-  }
-}
-
-// traverse(
-//   figma.root.findOne((n) => n.name === "ROOT") as unknown as SceneNode &
-//     ChildrenMixin
-// ); // start the traversal at the root
-
 const verify = (
   selection: readonly SceneNode[],
   cb: (selection: SceneNode) => void
