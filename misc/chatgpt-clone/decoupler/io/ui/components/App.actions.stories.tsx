9c49d70367b2efb240e373b321b91085aedc8416
nino-ux <nikita.novik.ux@gmail.com>
2023-06-12 14:13:22 +0300

    ChatGPT Clone: Create Action Driven Scenarios

--- misc/chatgpt-clone/decoupler/io/ui/components/App.actions.stories.tsx
+++ misc/chatgpt-clone/decoupler/io/ui/components/App.actions.stories.tsx
@@ -1,11 +1,10 @@
-import { Main } from "./App";
-import { EModel, EUser } from "../../../enums";
+import React from "react";
+import { mapStateToProps } from "..";
+import { initialState } from "../../../bridge";
+import { compose } from "../../../reducer";
 import { EActionType, EControlId, TAction, TMainProps } from "../../../types";
 import { ActionList } from "./ActionList";
-import React from "react";
-import { compose } from "../../../reducer";
-import { initialState } from "../../../bridge";
-import { mapStateToProps } from "..";
+import { Main } from "./App";
 
 const MainWithActions: React.FC<
   TMainProps & { showActions?: boolean; actions?: TAction<any>[] }
@@ -45,13 +44,13 @@ const SidePanelCollapsedActions: TAction<any>[] = [
     type: EActionType.Click,
   },
 ];
-const SidePanelCollapsedProps = compose(initialState)(
+const SidePanelCollapsedState = compose(initialState)(
   SidePanelCollapsedActions
 );
 export const SidePanelCollapsed = {
   args: {
     showActions: false,
-    ...mapStateToProps(SidePanelCollapsedProps),
+    ...mapStateToProps(SidePanelCollapsedState),
     actions: SidePanelCollapsedActions,
   },
 };
@@ -65,11 +64,11 @@ const ContainsInputActions: TAction<any>[] = [
     payload: "This is the input...",
   },
 ];
-const ContainsInputProps = compose(initialState)(ContainsInputActions);
+const ContainsInputState = compose(initialState)(ContainsInputActions);
 export const ContainsInput = {
   args: {
     showActions: false,
-    ...mapStateToProps(ContainsInputProps),
+    ...mapStateToProps(ContainsInputState),
     actions: ContainsInputActions,
   },
 };
@@ -82,11 +81,11 @@ const ToggleModelActions: TAction<any>[] = [
     type: EActionType.Click,
   },
 ];
-const ToggleModelProps = compose(initialState)(ToggleModelActions);
+const ToggleModelState = compose(initialState)(ToggleModelActions);
 export const ToggleModel = {
   args: {
     showActions: false,
-    ...ToggleModelProps,
+    ...mapStateToProps(ToggleModelState),
     actions: ToggleModelActions,
   },
 };
@@ -106,13 +105,13 @@ const ConversationAndMessagesActions: TAction<any>[] = [
     type: EActionType.Click,
   },
 ];
-const ConversationAndMessagesProps = compose(initialState)(
+const ConversationAndMessagesState = compose(initialState)(
   ConversationAndMessagesActions
 );
 export const ConversationAndMessages = {
   args: {
     showActions: false,
-    ...ConversationAndMessagesProps,
+    ...mapStateToProps(ConversationAndMessagesState),
     actions: ConversationAndMessagesActions,
   } as TMainProps,
 };
@@ -142,11 +141,11 @@ const ActiveMessageActions: TAction<any>[] = [
     },
   },
 ];
-const ActiveMessageProps = compose(initialState)(ActiveMessageActions);
+const ActiveMessageState = compose(initialState)(ActiveMessageActions);
 export const ActiveMessage = {
   args: {
     showActions: false,
-    ...ActiveMessageProps,
+    ...mapStateToProps(ActiveMessageState),
     actions: ActiveMessageActions,
   } as TMainProps,
 };
@@ -176,13 +175,192 @@ const FinalActiveMessageActions: TAction<any>[] = [
     },
   },
 ];
-const FinalActiveMessageProps = compose(initialState)(
+const FinalActiveMessageState = compose(initialState)(
   FinalActiveMessageActions
 );
 export const FinalActiveMessage = {
   args: {
     showActions: false,
-    ...FinalActiveMessageProps,
+    ...mapStateToProps(FinalActiveMessageState),
     actions: FinalActiveMessageActions,
+  } as TMainProps,
+};
+
+const LikeButtonActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.LikeButton,
+      uid: FinalActiveMessageState.messages?.at(-1)?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const LikeButtonState = compose(FinalActiveMessageState)(LikeButtonActions);
+export const LikeButton = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(LikeButtonState),
+    actions: LikeButtonActions,
+  } as TMainProps,
+};
+
+const DislikeButtonActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.DislikeButton,
+      uid: FinalActiveMessageState.messages?.at(-1)?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const DislikeButtonState = compose(FinalActiveMessageState)(
+  DislikeButtonActions
+);
+export const DislikeButton = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(DislikeButtonState),
+    actions: DislikeButtonActions,
+  } as TMainProps,
+};
+
+const NewChatClickActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.QueryInput,
+    },
+    type: EActionType.Change,
+    payload: "This is the input...",
+  },
+  {
+    id: {
+      id: EControlId.Submit,
+    },
+    type: EActionType.Click,
+  },
+  {
+    id: {
+      id: EControlId.QueryResponse,
+    },
+    type: EActionType.IO,
+    payload: {
+      text: "This is the only message I can generate...",
+      isDone: true,
+    },
+  },
+  {
+    id: {
+      id: EControlId.NewChat,
+    },
+    type: EActionType.Click,
+  },
+];
+const NewChatClickState = compose(initialState)(NewChatClickActions);
+export const NewChatClick = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(NewChatClickState),
+    actions: NewChatClickActions,
+  } as TMainProps,
+};
+
+const SelectConversationActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.Conversation,
+      uid: NewChatClickState.conversations[0].conversations?.[0]?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const SelectConversationState = compose(NewChatClickState)(
+  SelectConversationActions
+);
+export const SelectConversation = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(SelectConversationState),
+    actions: SelectConversationActions,
+  } as TMainProps,
+};
+
+const RemoveConversationActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.RemoveConversation,
+      uid: NewChatClickState.conversations[0].conversations?.[0]?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const RemoveConversationState = compose(SelectConversationState)(
+  RemoveConversationActions
+);
+export const RemoveConversation = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(RemoveConversationState),
+    actions: RemoveConversationActions,
+  } as TMainProps,
+};
+
+const EditConversationActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.EditConversation,
+      uid: NewChatClickState.conversations[0].conversations?.[0]?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const EditConversationState = compose(SelectConversationState)(
+  EditConversationActions
+);
+export const EditConversation = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(EditConversationState),
+    actions: EditConversationActions,
+  } as TMainProps,
+};
+
+const ConversationEditInputActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.ConversationEditInput,
+      uid: NewChatClickState.conversations[0].conversations?.[0]?.id,
+    },
+    type: EActionType.Change,
+    payload: "My cool conversation!",
+  },
+];
+const ConversationEditInputState = compose(EditConversationState)(
+  ConversationEditInputActions
+);
+export const ConversationEditInput = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(ConversationEditInputState),
+    actions: ConversationEditInputActions,
+  } as TMainProps,
+};
+
+const ConversationEditAcceptActions: TAction<any>[] = [
+  {
+    id: {
+      id: EControlId.ConversationEditAccept,
+      uid: NewChatClickState.conversations[0].conversations?.[0]?.id,
+    },
+    type: EActionType.Click,
+  },
+];
+const ConversationEditAcceptState = compose(ConversationEditInputState)(
+  ConversationEditAcceptActions
+);
+export const ConversationEditAccept = {
+  args: {
+    showActions: false,
+    ...mapStateToProps(ConversationEditAcceptState),
+    actions: ConversationEditAcceptActions,
   } as TMainProps,
 };
